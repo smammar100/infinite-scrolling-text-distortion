d989339eb946e6505a0c72d17126ea93296cd2e7
Jorge Toloza <jorgetloza@gmail.com>
2024-10-26 13:26:52 -0500

    :tada: Init

---
+++ src/js/pages/index.js
@@ -0,0 +1,10 @@
+import '../../styles/index.scss'
+import '../../styles/pages/index.scss'
+
+export default class Index {
+  constructor(options) {
+  }
+}
+window.addEventListener('load', () => {
+  new Index();
+});
